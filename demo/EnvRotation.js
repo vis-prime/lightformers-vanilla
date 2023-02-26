bae59de3398d29f7a3615c5ce513636599ed75b9
vis-prime <vishals1030@gmail.com>
2023-02-26 23:45:51 +0530

    Env Rotation example polish

--- demo/EnvRotation.js
+++ demo/EnvRotation.js
@@ -18,7 +18,6 @@ import {
   MeshBasicMaterial,
   PlaneGeometry,
   DirectionalLight,
-  RepeatWrapping,
   ShadowMaterial,
   VSMShadowMap,
   MathUtils,
@@ -26,14 +25,57 @@ import {
 import Stats from "three/examples/jsm/libs/stats.module"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader"
+import { EXRLoader } from "three/examples/jsm/loaders/EXRLoader"
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { TransformControls } from "three/examples/jsm/controls/TransformControls"
 import { GroundProjectedEnv } from "./CustomGroundProjectedEnv"
 
-import hdriURL from "../public/between_bridges_1k.hdr?url"
-import hdriWebPURL from "../public/between_bridges1.webp?url"
+// files
 import modelUrl from "../public/rolls_royce_ghost.glb"
+
+/**
+ * Env/bg/sun options
+ * @enum
+ * @type {Object}
+ */
+const EnvOptions = {
+  between_bridges: {
+    env: "../public/between_bridges_1k.hdr",
+    bg: "../public/between_bridges1.webp",
+    sunPos: [10, 5, 8],
+    sunCol: "#ffffeb",
+    shadowOpacity: 0.5,
+    groundProj: { radius: 50, height: 5 },
+  },
+  wide_street_1: {
+    env: "../public/wide_street_01_1k.exr",
+    bg: "../public/wide_street_01.webp",
+    isExr: true,
+    sunPos: [15, 24, 11],
+    sunCol: "#ffffeb",
+    shadowOpacity: 0.85,
+    groundProj: { radius: 12, height: 2 },
+  },
+  ulmer_muenster: {
+    env: "../public/ulmer_muenster_1k.exr",
+    bg: "../public/ulmer_muenster.webp",
+    isExr: true,
+    sunPos: [17, 14, 12],
+    sunCol: "#ffffeb",
+    shadowOpacity: 0.72,
+    groundProj: { radius: 25, height: 2 },
+  },
+  wide_street_2: {
+    env: "../public/wide_street_02_1k.exr",
+    bg: "../public/wide_street_02.webp",
+    isExr: true,
+    sunPos: [16, 8, 12],
+    sunCol: "#ffffeb",
+    shadowOpacity: 0.55,
+    groundProj: { radius: 25, height: 2 },
+  },
+}
 
 let stats,
   renderer,
@@ -44,45 +86,51 @@ let stats,
   gui,
   pointer = new Vector2()
 
+let sceneGui
+let envObject, gpE, sunGroup, sunLight, envFolder, shadowFloor
+let transformControls
+
 const params = {
   bgColor: new Color(),
+  env: EnvOptions.between_bridges,
   envRotation: 0,
+  gpRad: 1,
+  gpHeight: 50,
   animateRotation: false,
   rotEnv: () => {},
   printCam: () => {},
 }
+
 const mainObjects = new Group()
 const textureLoader = new TextureLoader()
 const rgbeLoader = new RGBELoader()
+const exrLoader = new EXRLoader()
 const gltfLoader = new GLTFLoader()
 const draco = new DRACOLoader()
-let transformControls
+
 // draco.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.5.5/")
 draco.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/")
 gltfLoader.setDRACOLoader(draco)
 const raycaster = new Raycaster()
 const intersects = [] //raycast
 
-let sceneGui
-let envObject, gpv, dirGroup
-
-let car,
-  sphere,
-  wheels = {
-    FL: null,
-    FR: null,
-    RL: null,
-    RR: null,
-    steerL: null,
-    steerR: null,
-    onAnimate: () => {},
-  }
+// wheel references
+const wheels = {
+  FL: null,
+  FR: null,
+  RL: null,
+  RR: null,
+  steerL: null,
+  steerR: null,
+  onAnimate: () => {},
+}
 
 /**
  * PMREM
  * @type {PMREMGenerator}
  */
 let pmremGenerator
+
 export async function initEnvRot(mainGui) {
   gui = mainGui
   sceneGui = gui.addFolder("Scene")
@@ -111,68 +159,13 @@ export async function initEnvRot(mainGui) {
   camera.position.set(6, 3, 6)
   camera.name = "Camera"
   camera.position.set(2.0, 2.64, 3.86)
+
+  if (camera.aspect < 1) {
+    // phones
+    camera.position.multiplyScalar(2)
+  }
   // scene
   scene = new Scene()
-  //   scene.backgroundBlurriness = 0.8
-
-  // light
-  dirGroup = new Group()
-  const dirLight = new DirectionalLight(0xffffeb, 1)
-  dirLight.name = "Dir. Light"
-  dirLight.position.set(10, 10, 10)
-  dirLight.castShadow = true
-  dirLight.shadow.camera.near = 0.1
-  dirLight.shadow.camera.far = 50
-  dirLight.shadow.camera.right = 15
-  dirLight.shadow.camera.left = -15
-  dirLight.shadow.camera.top = 15
-  dirLight.shadow.camera.bottom = -15
-  dirLight.shadow.mapSize.width = 1024
-  dirLight.shadow.mapSize.height = 1024
-  dirLight.shadow.radius = 1
-
-  dirLight.shadow.bias = -0.0005
-  dirGroup.add(dirLight)
-  scene.add(dirGroup)
-
-  envObject = new Mesh(
-    new SphereGeometry(1).scale(1, 1, -1),
-    new MeshBasicMaterial()
-  )
-
-  rgbeLoader.load(hdriURL, (texture) => {
-    texture.mapping = EquirectangularReflectionMapping
-    envObject.material.map = texture
-
-    scene.environment = pmremGenerator.fromScene(envObject).texture
-  })
-
-  gui.add(params, "animateRotation").name("⚠ Animate Rotation")
-  gui
-    .add(params, "envRotation", -Math.PI, Math.PI, 0.05)
-    .name("🌏Env Rotation")
-    .onChange((v) => {
-      rotateEnv()
-    })
-
-  gui.add(dirLight.shadow, "radius").name("💡radius").min(0).max(25)
-  gui.add(dirLight.shadow, "blurSamples", 1, 25, 1).name("💡samples")
-  gui.addColor(dirLight, "color").name("💡Color")
-  gui.add(dirLight, "intensity", 0, 25, 0.01).name("💡Intensity")
-
-  textureLoader.load(hdriWebPURL, (texture) => {
-    texture.encoding = sRGBEncoding
-    texture.mapping = EquirectangularReflectionMapping
-    texture.magFilter = LinearFilter
-    texture.minFilter = LinearFilter
-    gpv = new GroundProjectedEnv(texture, { height: 5, radius: 50 })
-    gpv.scale.setScalar(100)
-    gui.add(gpv, "height", 1, 100).name("Ground Height")
-    gui.add(gpv, "radius", 1, 100).name("Ground Radius")
-
-    scene.add(gpv)
-  })
-
   scene.add(mainObjects)
 
   // controls
@@ -202,13 +195,13 @@ export async function initEnvRot(mainGui) {
   scene.add(transformControls)
 
   window.addEventListener("resize", onWindowResize)
-  document.addEventListener("pointermove", onPointerMove)
+  app.addEventListener("pointermove", onPointerMove)
 
   let downTime = Date.now()
-  document.addEventListener("pointerdown", () => {
+  app.addEventListener("pointerdown", () => {
     downTime = Date.now()
   })
-  document.addEventListener("pointerup", (e) => {
+  app.addEventListener("pointerup", (e) => {
     if (Date.now() - downTime < 200) {
       onPointerMove(e)
       raycast()
@@ -216,20 +209,177 @@ export async function initEnvRot(mainGui) {
   })
 
   sceneGui.add(transformControls, "mode", ["translate", "rotate", "scale"])
-  sceneGui.add(scene, "backgroundBlurriness", 0, 1, 0.01)
-  sceneGui.addColor(params, "bgColor").onChange(() => {
-    scene.background = params.bgColor
-  })
 
+  setupEnv()
   await loadModels()
   animate()
 }
 
+function setupEnv() {
+  // floor
+  shadowFloor = new Mesh(
+    new PlaneGeometry(10, 10).rotateX(-Math.PI / 2),
+    new ShadowMaterial({ opacity: 0.8 })
+  )
+  shadowFloor.name = "shadow_floor"
+  shadowFloor.receiveShadow = true
+  shadowFloor.position.set(0, -0.001, 0)
+  mainObjects.add(shadowFloor)
+
+  // light
+  sunGroup = new Group()
+  sunLight = new DirectionalLight(0xffffeb, 1)
+  sunLight.name = "Dir. Light"
+  sunLight.castShadow = true
+  sunLight.shadow.camera.near = 0.1
+  sunLight.shadow.camera.far = 50
+  sunLight.shadow.camera.right = 15
+  sunLight.shadow.camera.left = -15
+  sunLight.shadow.camera.top = 15
+  sunLight.shadow.camera.bottom = -15
+  sunLight.shadow.mapSize.width = 1024
+  sunLight.shadow.mapSize.height = 1024
+  sunLight.shadow.radius = 1.95
+  sunLight.shadow.blurSamples = 6
+
+  sunLight.shadow.bias = -0.0005
+  sunGroup.add(sunLight)
+  scene.add(sunGroup)
+
+  envObject = new Mesh(
+    new SphereGeometry(1).scale(1, 1, -1),
+    new MeshBasicMaterial()
+  )
+
+  updateEnv(params.env)
+}
+
+function updateEnvGui() {
+  if (envFolder) {
+    for (const fol of [...envFolder.children]) {
+      fol.destroy()
+    }
+  } else {
+    envFolder = gui.addFolder("🌍ENV/BG/Light")
+    envFolder.open()
+  }
+
+  envFolder
+    .add(params, "env", EnvOptions)
+    .name("🌏Select Env")
+    .onChange((v) => {
+      updateEnv(v)
+    })
+  envFolder.add(params, "animateRotation").name("⚠ Animate Rotation")
+  envFolder
+    .add(params, "envRotation", 0, 2 * Math.PI, 0.05)
+    .name("🌏Env Rotation")
+    .onChange((v) => {
+      rotateEnv()
+    })
+
+  envFolder.add(sunLight.shadow, "radius").name("💡radius").min(0).max(25)
+  // envFolder.add(sunLight.position, "x").onChange(() => {
+  //   console.log(sunLight.position.toArray())
+  // })
+  // envFolder.add(sunLight.position, "y").onChange(() => {
+  //   console.log(sunLight.position.toArray())
+  // })
+  // envFolder.add(sunLight.position, "z").onChange(() => {
+  //   console.log(sunLight.position.toArray())
+  // })
+  envFolder.add(sunLight.shadow, "blurSamples", 1, 25, 1).name("💡samples")
+  envFolder.addColor(sunLight, "color").name("💡Color")
+  envFolder.add(sunLight, "intensity", 0, 25, 0.01).name("💡Intensity")
+  envFolder
+    .add(shadowFloor.material, "opacity", 0, 1, 0.01)
+    .name("💡Shadow opacity")
+
+  envFolder
+    .add(params, "gpHeight", 1, 100)
+    .name("Ground Height")
+    .onChange((v) => {
+      if (gpE) {
+        gpE.height = v
+      }
+    })
+  envFolder
+    .add(params, "gpRad", 1, 100)
+    .name("Ground Radius")
+    .onChange((v) => {
+      if (gpE) {
+        gpE.radius = v
+      }
+    })
+}
+
+/**
+ * Update Current env/bg combo
+ * @param {EnvOptions} envDict
+ */
+async function updateEnv(envDict) {
+  const { env, bg, isExr, sunPos, sunCol, groundProj, shadowOpacity } = envDict
+
+  if (sunPos) {
+    sunLight.visible = true
+    sunLight.position.fromArray(sunPos)
+  } else {
+    sunLight.visible = false
+  }
+
+  if (sunCol) {
+    sunLight.color.set(sunCol)
+  } else {
+    sunLight.color.set(0xffffff)
+  }
+
+  const envTexturePromise = isExr
+    ? exrLoader.loadAsync(env)
+    : rgbeLoader.loadAsync(env)
+  const bgTexturePromise = textureLoader.loadAsync(bg)
+
+  const [envTexture, bgTexture] = await Promise.all([
+    envTexturePromise,
+    bgTexturePromise,
+  ])
+
+  envTexture.mapping = EquirectangularReflectionMapping
+  envObject.material.map = envTexture
+
+  bgTexture.encoding = sRGBEncoding
+  bgTexture.mapping = EquirectangularReflectionMapping
+  bgTexture.magFilter = LinearFilter
+  bgTexture.minFilter = LinearFilter
+
+  if (groundProj) {
+    params.gpRad = groundProj.radius
+    params.gpHeight = groundProj.height
+  }
+
+  if (!gpE) {
+    gpE = new GroundProjectedEnv(bgTexture)
+    gpE.scale.setScalar(100)
+    scene.add(gpE)
+  }
+
+  if (shadowOpacity) {
+    shadowFloor.material.opacity = shadowOpacity
+  }
+
+  gpE.material.uniforms.map.value = bgTexture
+  gpE.height = params.gpHeight
+  gpE.radius = params.gpRad
+
+  rotateEnv()
+  updateEnvGui()
+}
+
 function rotateEnv() {
-  dirGroup.rotation.y = params.envRotation
-  gpv.angle = params.envRotation
+  sunGroup.rotation.y = params.envRotation
+  gpE.angle = params.envRotation
 
   envObject.rotation.y = params.envRotation
+  if (scene.environment) scene.environment.dispose()
   scene.environment = pmremGenerator.fromScene(envObject).texture
 }
 
@@ -301,26 +451,18 @@ async function loadModels() {
   sphere.raycastRoot = sphere
   mainObjects.add(sphere)
 
-  // floor
-  const plane = new Mesh(
-    new PlaneGeometry(10, 10).rotateX(-Math.PI / 2),
-    new ShadowMaterial({ opacity: 0.8 })
-  )
-  plane.name = "plane"
-  plane.receiveShadow = true
-  plane.position.set(0, -0.001, 0)
-  mainObjects.add(plane)
-  gui.add(plane.material, "opacity", 0, 1, 0.01).name("💡Shadow opacity")
-
   // car
   const gltf = await gltfLoader.loadAsync(modelUrl)
   const model = gltf.scene
   model.name = "car"
+
+  const mats = {}
   model.traverse((child) => {
     if (child.isMesh) {
       child.raycastRoot = model
       child.castShadow = true
       child.receiveShadow = true
+      mats[child.material.name] = child.material
     }
   })
   mainObjects.add(model)
@@ -332,21 +474,6 @@ async function loadModels() {
   wheels.steerL = model.getObjectByName("steer_l")
   wheels.steerR = model.getObjectByName("steer_r")
 
-  const carParams = {
-    wheelSpeed: 0.001,
-    steer: 0,
-  }
-  const steerLimit = MathUtils.degToRad(30)
-
-  gui.add(carParams, "wheelSpeed", 0, 0.1).name("🚗 speed")
-  gui
-    .add(carParams, "steer", -1, 1, 0.1)
-    .name("🚗 steer")
-    .onChange((v) => {
-      const rotY = MathUtils.mapLinear(v, -1, 1, -steerLimit, steerLimit)
-      wheels.steerL.rotation.y = rotY
-      wheels.steerR.rotation.y = rotY
-    })
   wheels.onAnimate = () => {
     if (carParams.wheelSpeed === 0) return
     wheels.FL.rotation.x += carParams.wheelSpeed
@@ -354,6 +481,32 @@ async function loadModels() {
     wheels.RL.rotation.x += carParams.wheelSpeed
     wheels.RR.rotation.x += carParams.wheelSpeed
   }
+
+  const carParams = {
+    wheelSpeed: 0.001,
+    steer: 0,
+  }
+
+  let body1Mat = mats["body_1"]
+  let body2Mat = mats["body_2"]
+
+  const steerLimit = MathUtils.degToRad(30)
+  const carFolder = gui.addFolder("🚗")
+  carFolder.add(carParams, "wheelSpeed", 0, 0.1).name("🚗 speed")
+  carFolder
+    .add(carParams, "steer", -1, 1, 0.1)
+    .name("🚗 steer")
+    .onChange((v) => {
+      const rotY = MathUtils.mapLinear(v, -1, 1, -steerLimit, steerLimit)
+      wheels.steerL.rotation.y = rotY
+      wheels.steerR.rotation.y = rotY
+    })
+  carFolder.addColor(body1Mat, "color").name("Color 1")
+  carFolder.addColor(body2Mat, "color").name("Color 2")
+  carFolder.add(body1Mat, "metalness", 0, 1, 0.001).name("Metal 1")
+  carFolder.add(body2Mat, "metalness", 0, 1, 0.001).name("Metal 2")
+  carFolder.add(body1Mat, "roughness", 0, 1, 0.001).name("Rough 1")
+  carFolder.add(body2Mat, "roughness", 0, 1, 0.001).name("Rough 2")
 }
 
 const color = new Color()
